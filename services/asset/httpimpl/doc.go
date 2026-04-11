18d9e263132f2fb555c451460d098889ce0ef16f
gokhan-sagirlar <gokhan.sagirlar@coinbase.com>
2026-04-11 14:56:32 +0300

    asset service swagger impl

---
+++ services/asset/httpimpl/doc.go
@@ -0,0 +1,22 @@
+// Package httpimpl Teranode Asset Service API.
+//
+// REST API for accessing blockchain data including blocks, transactions,
+// subtrees, UTXOs, and merkle proofs. Supports JSON, hexadecimal, and
+// binary response formats.
+//
+//	Schemes: http, https
+//	Host: localhost:8080
+//	BasePath: /api/v1
+//	Version: 1.0.0
+//	License: Open BSV License https://github.com/bsv-blockchain/teranode/blob/master/LICENSE
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//	- application/octet-stream
+//	- text/plain
+//
+// swagger:meta
+package httpimpl
